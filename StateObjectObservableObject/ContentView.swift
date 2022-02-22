3ffb5eb7bf4f3ff0153e2f23363fa0a3eee5365e
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2022-02-22 20:05:56 +0200

    Initial Commit

---
+++ StateObjectObservableObject/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  StateObjectObservableObject
+//
+//  Created by ramil on 22.02.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ StateObjectObservableObject/StateObjectObservableObjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  StateObjectObservableObjectApp.swift
+//  StateObjectObservableObject
+//
+//  Created by ramil on 22.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct StateObjectObservableObjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
